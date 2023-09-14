cd5fb919b000382dfa5227043223152b15ff9bea
AmirmahdiShayankhwah <amyrmhdyshaya7@gmail.com>
2023-09-14 13:48:53 +0330

    first commit

---
+++ Controllers/Hotel.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class Hotel : Controller
+{
+    public IActionResult indexi()
+    {
+        return View ();
+    }
+    public IActionResult Restoran()
+    {
+        return View ();
+    }
+    public IActionResult Room()
+    {
+        return View ();
+    }
+    public IActionResult Contact()
+    {
+        return View ();
+    }
+    
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Hotelmvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
